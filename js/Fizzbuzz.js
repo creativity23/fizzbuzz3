40d08c95c4719d8c85c0a03f0eba945591ea732e
creativity23 <abiracal@yahoo.com>
2021-01-13 01:53:43 -0500

    first commit

---
+++ js/Fizzbuzz.js
@@ -0,0 +1,37 @@
+//Step1: Wire up an event handler for the fizz buzz button
+document.getElementById("btn-Submit").addEventListener("click", function(){
+    //Step2: Store the users input into a local variable named userWord
+    let input1= parseInt(document.getElementById("input1").value);
+    let input2= parseInt(document.getElementById("input2").value);
+    let storage = [];
+     console.log(input1);
+    for (let loop = 1;loop <= 100; loop++) {
+        if(loop % input1==0 && loop % input2 == 0) {
+            storage.push("fizzbuzz");
+        }   
+        else if (loop % input1 == 0){
+            storage.push ("fizz");
+        }
+        else if (loop % input2 == 0){
+            storage.push("buzz");
+        }
+        else {
+            storage.push(loop);
+        } 
+               
+                
+    }
+    document.getElementById("output").innerText = storage.join(",");
+
+
+
+
+
+
+
+
+
+
+
+
+});
